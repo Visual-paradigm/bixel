65a47fef8e7a2871d3946f9818a498cdb65300bc
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2020-05-07 08:45:33 -0500

    gray_scale video!

---
+++ Sketches/gray_scale/Video/grayscale_video.js
@@ -0,0 +1,49 @@
+
+var video;
+
+
+function setup() {
+  
+    var cnv = createCanvas(645, 500);
+    cnv.position('190', '570');
+    background(51);
+    video = createVideo(['../Sketches/gray_scale/Video/video.mp4']);
+    video.size(640, 480);
+    video.position('610','670')
+    
+}
+
+function draw() {
+    
+    background(255);
+    video.loadPixels();
+    const stepSize =  1;
+    for (let y = 0; y < height; y += stepSize) {
+        for (let x = 0; x < width; x += stepSize) {
+        const i = y * width + x;
+        const darkness = (255 - video.pixels[i * 4]) / 255;
+        const radius = stepSize * darkness;
+        let r = video.pixels[i*4];
+        let g = video.pixels[(i*4)+1]
+        let b = video.pixels[(i*4)+2];
+        let luma = 0.299*r+0.587*g+0.114*b;
+        video.pixels[(i*4)]= luma;      
+        video.pixels[(i*4)+1]= luma;     
+        video.pixels[(i*4)+2]= luma;
+        }
+    }  
+    video.updatePixels();  
+  
+  
+    image(video, 0, 0, 640, 480);
+}
+
+function keyPressed() {
+    if(key=='a'){
+        video.pause();
+      }
+    if(key=='p'){
+        video.loop();
+    }
+  
+}
---
+++ _posts/Procesamiento de imágenes/gray_scale/2020-04-24-grayscaleV.md
@@ -0,0 +1,24 @@
+---
+layout: post
+title: "Implementación y visualización"
+date: 2019-11-05 08:44:38 -0400
+category: grayscale
+author: mac
+short-description: Escala de grises para un video
+---
+## Escala de grises aplicado a un video
+
+
+**Presiona 'p' para reproducir el video
+y 'a' para pausar la reproducción**
+
+
+
+#### El resultado obtenido es el siguiente: 
+
+.
+
+![_config.yml]({{ site.baseurl }}/Sketches/gray_scale/Video/via.png)
+<script src="../p5.js"></script>
+<script src="../Sketches/gray_scale/Video/grayscale_video.js"></script>
+
